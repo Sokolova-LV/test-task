7671705bbd5863260e9df91b7420384e760fe8c8
Sokolova-LV <leyla-@ukr.net>
2024-03-13 09:07:38 +0200

    update js

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Находим ссылку с классом main__link-click
     var links = document.querySelectorAll('.main__link-click');
 
-    // Добавляем обработчик события клика для ссылки
     links.forEach(function(link) {
         link.addEventListener('click', function(e) {
-            e.preventDefault(); // Предотвращаем переход по ссылке
-            changeLogoTitle(); // Вызываем функцию для смены логотипа на текст
+            e.preventDefault(); 
+            changeLogoTitle(); 
         });
     });
 
-    // Функция для смены логотипа на текст
+    // change logo
     function changeLogoTitle() {
         var logo = document.querySelector('.main__logo');
         var title = document.querySelector('.main__title');
 
-        // Скрываем логотип и показываем текст
+        // hide logo, display text
         logo.style.display = 'none';
         title.style.display = 'block';
     }
